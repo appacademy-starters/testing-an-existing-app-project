b673da993983dd8f3578863c868179ca3699a1ea
Raymond <raymond.may.95@outlook.com>
2020-11-13 17:57:09 -1000

    get-value-from-body completed

--- test/get-value-from-body-spec.js
+++ test/get-value-from-body-spec.js
@@ -1,82 +1,40 @@
-// const { expect } = require('chai');
-// const { getValueFromBody } = require('../get-value-from-body');
+const { expect } = require('chai');
+const { getValueFromBody } = require('../get-value-from-body');
 
-// describe("The getValueFromBody function", () => {
-//   it('returns an empty string for an empty body', () => {
-//     // Arrange
-//     const body = "";
-//     const key = "notThere";
+describe("The getValueFromBody function", () => {
+  it('returns an empty string for an empty body', () => {
+    const body = "";
+    const key = "notThere";
+    const res = getValueFromBody(key)
+    expect(res).to.eql(body);
+  });
 
-//     // Act
-//     // Write code to invoke getValueFromBody and collect
-//     // the result
+  it('returns an empty string for a body without the key', () => {
+    const body = "name=Bess&age=29&job=Boss";
+    const key = "notThere";
+    const res = getValueFromBody(body)
+    expect(res).to.eql('');
+  });
 
+  it('returns the value of the key in a simple body', () => {
+    const body = "name=Bess";
+    const key = "name";
+   const res = getValueFromBody(body, key)
+    expect(res).to.eql('Bess');
+  });
 
-//     // Assert
-//     // Replace the fail line with an assertion for the
-//     // expected value of ""
-//     expect.fail('please write this test');
-//   });
+  it('returns the value of the key in a complex body', () => {
+    const body = "name=Bess&age=29&job=Boss";
+    const key = "name";
+    const res = getValueFromBody(body, key)
 
-//   it('returns an empty string for a body without the key', () => {
-//     // Arrange
-//     const body = "name=Bess&age=29&job=Boss";
-//     const key = "notThere";
+    expect(res).to.eql('Bess');
+  });
 
-//     // Act
-//     // Write code to invoke getValueFromBody and collect
-//     // the result
-
-
-//     // Assert
-//     // Replace the fail line with an assertion for the
-//     // expected value of ""
-//     expect.fail('please write this test');
-//   });
-
-//   it('returns the value of the key in a simple body', () => {
-//     const body = "name=Bess";
-//     const key = "name";
-
-//     // Act
-//     // Write code to invoke getValueFromBody and collect
-//     // the result
-
-
-//     // Assert
-//     // Replace the fail line with an assertion for the
-//     // expected value of "Bess"
-//     expect.fail('please write this test');
-//   });
-
-//   it('returns the value of the key in a complex body', () => {
-//     const body = "name=Bess&age=29&job=Boss";
-//     // Select one of the keys in the body
-
-//     // Act
-//     // Write code to invoke getValueFromBody and collect
-//     // the result
-
-
-//     // Assert
-//     // Replace the fail line with an assertion for the
-//     // expected value for the key that you selected
-//     expect.fail('please write this test');
-//   });
-
-//   it('decodes the return value of URL encoding', () => {
-//     const body = "name=Bess&age=29&job=Boss&level=Level%20Thirty-One";
-//     const key = "level";
-
-//     // Act
-//     // Write code to invoke getValueFromBody and collect
-//     // the result
-
-
-//     // Assert
-//     // Replace the fail line with an assertion for the
-//     // expected value for the key that you selected that
-//     // has the encoded value in it
-//     expect.fail('please write this test');
-//   });
-// });
+  it('decodes the return value of URL encoding', () => {
+    const body = "name=Bess&age=29&job=Boss&level=Level%20Thirty-One";
+    const key = "level";
+    const res = getValueFromBody(body, key)
+    expect(res).to.eql('Level Thirty-One');
+  });
+});
